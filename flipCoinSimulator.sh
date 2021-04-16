a14b3886a27a04d25d713455b6428e109bff1319
prudhvi <prudhvieethakota123@gmail.com>
2021-04-17 00:34:47 +0530

    Added:Loop Through Multiple times of Flipping a Coin and show the number of times Head and Tail has Won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,12 +3,22 @@
 echo "Flip Coin Simulation"
 echo "This problem Displays the Winner Heads or Tails"
 
-randomNumber=$((RANDOM%2))
-echo $randomNumber
+Head=0
+Tail=0
+for (( i=1; i<=20; i++ ))
+do
+   randomNumber=$((RANDOM%2))
+   echo $randomNumber
 
-if [ $randomNumber -eq 1 ]
-then 
-    echo "Head is a Winner"
-else 
-    echo "Tail is a Winner "
-fi
+      if [ $randomNumber  -eq 1 ]
+      then 
+          echo "Head is a Winner"
+          ((Head++))
+      else 
+          echo "Tail is a Winner "
+          ((Tail++))
+      fi
+done
+
+echo "No Of times Head Won = $Head"
+echo "No of times Tail Won = $Tail"
